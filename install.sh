579b55752298ed366b0874c74ec704bf58337042
hath995 <hathlesstroubles@gmail.com>
2015-11-11 01:06:00 +0000

    Adding new nvim config commands

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 ln -s ~/.vim/.vimrc ~/.vimrc
 ln -s ~/.vim/.screenrc ~/.screenrc
+mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
+ln -s ~/.vim $XDG_CONFIG_HOME/nvim
+ln -s ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
 vim +PluginInstall +qall
 
