2b307ef7de566ca03071905502d019204f846c3d
hath995 <hathlesstroubles@gmail.com>
2016-10-19 04:42:14 +0000

    remove bad if

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
 ln -s ~/.vim/vimrc ~/.vimrc
 ln -s ~/.vim/.screenrc ~/.screenrc
-if [ "$(uname)" == "Darwin" ]; then
-  mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
-  ln -s ~/.vim $XDG_CONFIG_HOME/nvim
-  ln -s ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
-fi
+mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
+ln -s ~/.vim $XDG_CONFIG_HOME/nvim
+ln -s ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
 vim -u bundlerc.vim +PluginInstall +qall
 
